04e65a4349908515eef716b0373010658dc2a2a6
Omar-zoubi <o.fandi02@gmail.com>
2021-04-20 13:10:15 +0300

    kk

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS charac(
+    id SERIAL,
+    character VARCHAR(250),
+   image VARCHAR(250),
+   quote VARCHAR(250), 
+   characterDirection VARCHAR(250)
+)
--- server.js
+++ server.js
@@ -14,9 +14,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Express middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.use(methodOverride('_method'))
 // Utilize ExpressJS functionality to parse the body of the request
 
 // Specify a directory for static resources
+
 
 // define our method-override reference
 
@@ -30,11 +35,69 @@ const client = new pg.Client(process.env.DATABASE_URL);
 
 // app routes here
 // -- WRITE YOUR ROUTES HERE --
+app.get('/', renderTen);
+app.post('/savetodata', saveToDataBase);
+app.get('/favorite-quotes', renderDataFromDB);
+app.put('/update/:id', updateData);
+app.delete('/update/:id', deleteData);
+
 
 
 // callback functions
 // -- WRITE YOUR CALLBACK FUNCTIONS FOR THE ROUTES HERE --
+function saveToDataBase(req, res)
+{
+    const {character, image, quote }=req.body;
+    const SQL= 'INSERT INTO charac (character, image, quote) VALUES($1, $2, $3);'
+    let val=[character, image, quote];
+    client.query(SQL, val).then(()=>{
+        res.redirect('/favorite-quotes');
+    })
+}
+function renderDataFromDB(req, res)
+{
+    const SQL="SELECT * FROM charac;"
+    client.query(SQL).then(data=>{
 
+        const alval=data.rows;
+        res.render('secondPage', {alval:alval});
+    });
+}
+
+function updateData(req, res)
+{
+    const {character,image, quote, characterDirection}=req.body;
+    const id=req.params.id;
+    const SQL ="UPDATE charac SET character=$1, image=$2, quote=$3, characterDirection=$4 WHERE id= $5;"
+    const val=[character,image, quote, characterDirection, id];
+    client.query(SQL, val).then(()=>{
+        res.redirect('/favorite-quotes');
+    })
+}
+function deleteData(req, res)
+{
+    
+    const SQL=`DELETE FROM charac WHERE id=${req.params.id} `
+    client.query(SQL).then(()=>{
+        res.redirect('/favorite-quotes');
+})}
+function Sin(data)
+{
+    this.character=data.character;
+    this.image=data.image;
+    this.quote=data.quote;
+    this.characterDirection=data.characterDirection;
+}
+
+function renderTen(req, res)
+{
+    const url='https://thesimpsonsquoteapi.glitch.me/quotes?count=10';
+        superagent.get(url).set('User-Agent', '1.0').then(data=>{
+            const returneddata= data.body.map(element=> new Sin(element));
+            res.render('index', {returneddata:returneddata});
+        })
+
+}
 // helper functions
 
 // app start point
